3196573c41ea23076fca1fc18a5e04e9ef228fcf
Zakariae <zakariaelhichem@gmail.com>
2022-07-20 19:35:17 +0000

    Add repositories

---
+++ src/main/java/com/bank/kata/repositories/OperationRepository.java
@@ -0,0 +1,9 @@
+package com.bank.kata.repositories;
+
+import com.bank.kata.entities.Operation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OperationRepository extends JpaRepository<Operation, Long> {
+}
